21489df407c7345b4ca7de52c5bab061cfeecb5a
ruiyotabby <ruiyot@DESKTOP-JJ0RO35.localdomain>
2022-09-22 12:13:53 +0300

    Add a function which takes a word(s) then checks in an array of substrings

---
+++ substrings.rb
@@ -0,0 +1,15 @@
+require 'pry-byebug'
+
+def substrings(word, dictionary)
+   dictionary.reduce(Hash.new(0)) do |total, dic|
+    word.split.each do |word_arr|
+    total[dic] += 1 if word_arr.downcase.match?(dic)
+    end
+    total
+   end
+
+end
+
+dictionary = ["below","down","be","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+p substrings("below", dictionary)
